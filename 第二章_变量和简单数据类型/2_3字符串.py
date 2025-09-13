a9d3bfc7e70c4ada2912a1af1bef63288de07cd4
Geraintblanc <1157473224@qq.com>
2025-09-13 14:26:13 +0800

    Initial commit

--- 第二章_变量和简单数据类型/2_3字符串.py
+++ 第二章_变量和简单数据类型/2_3字符串.py
@@ -1,3 +1,15 @@
 ###字符串
 # 字符串就是一系列字符，在python中，用引号括起的都是字符串，
 # 其中的引号可以是单引号也可以是双引号
+# 2.3.1 使用方法修改字符串的大小写
+### 1 改变首字母大小写
+name = 'ada lovelace'
+print(name.title())
+# Ada Lovelace
+
+### 2 全部变成大写
+print(name.upper())
+# ADA LOVELACE
+### 3. 全部变成小写
+print(name.lower())
+#ada lovelace
