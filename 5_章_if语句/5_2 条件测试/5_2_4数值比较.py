0419d974b3654ac04792738db653c77e98e1cc92
Geraintblanc <1157473224@qq.com>
2025-09-14 23:02:52 +0800

    submit

---
+++ 5_章_if语句/5_2 条件测试/5_2_4数值比较.py
@@ -0,0 +1,10 @@
+# ⭐ 5.2.4 数值比较
+# 检查数值非常简单
+age = 18
+age == 18
+print(age == 18) # True
+print('-------------下面的例子检查两个数是否不等-------------')
+
+answer = 17
+if answer != 42:
+    print("That is not the correct answer. Please try again!")
