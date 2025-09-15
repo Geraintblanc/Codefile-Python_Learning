98baa8aa5d01935413224c66a5279977c0558733
Geraintblanc <1157473224@qq.com>
2025-09-16 06:22:23 +0800

    submit

---
+++ 7_章_用户输入和while循环/7_2_while循环简介/7_2_1_使用while循环.py
@@ -0,0 +1,13 @@
+# 🌙 7.2 while 循环简介
+"""
+for 循环和while循环的区别：
+    for循环用于针对集合中的灭个元素都执行一个代码块，而while训话你则不断运行，
+    直到指定的条件不满足为止。
+"""
+# 😀 7.2.1 使用while循环
+# 可使用while循环来数数，如while循环从1数到5：
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1 # 1，2，3，4，5
+
